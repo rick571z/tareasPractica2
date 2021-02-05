83994d7bb75ab07cf90e45c29f78423b2dbe7fa4
Fire <wilmer_fi@hotmail.com>
2021-02-05 17:40:39 +0100

    funcion para guardar una tarea  en tareas.js

--- js/tareas.js
+++ js/tareas.js
@@ -9,7 +9,6 @@ let contador = 0;
 let miprioridad;
 let conterror = 0;
 
-//imprime el formato del articulo
 const printListado = function(pListadoTareas) {
 
     selectionListado.innerHTML = "";
@@ -31,3 +30,66 @@ const printListado = function(pListadoTareas) {
 }
 
 printListado(listaTareas);
+
+let eliminarTarea = document.querySelector('#tarea');
+
+let selectPrioridad = document.querySelector('#seleccion');
+
+selectPrioridad.addEventListener('change', getPrioridad);
+
+let inputName = document.querySelector('#texto');
+
+let guardarTarea = document.querySelector('#btn-guardar');
+
+//evento para guardar prioridad
+inputName.addEventListener('keydown', guardarNuevaTarea);
+guardarTarea.addEventListener('click', guardarNuevaTarea);
+
+function getPrioridad(event) {
+    miprioridad = event.target.value;
+    //console.log("la priorieadad es:" + miprioridad);
+
+}
+
+//longitud de la lista
+function longitudLista(miLista) {
+    let cont = 0;
+    for (let i = 0; i < miLista.length; i++) {
+        cont++;
+    }
+    return cont;
+}
+
+//corregir el mensaje del borrado.?
+function guardarNuevaTarea(event) {
+
+    if (event.type == 'click') {
+
+        let tarea = inputName.value;
+        console.log("mi tarea: " + tarea);
+        let contador = longitudLista(listaTareas);
+
+        if ((miprioridad == 'urgente' || miprioridad == 'diaria' || miprioridad == 'mensual') && (tarea != "")) {
+            //creacion del objeto
+            let file1 = {
+                idTarea: contador,
+                titulo: tarea,
+                prioridad: miprioridad
+            }
+            selectAviso.innerHTML = "";
+
+            listaTareas.push(file1);
+
+        } else if (tarea != "" || miprioridad == undefined || miprioridad == "") {
+            selectAviso.innerHTML = `<p>${'TIENES QUE INGRESAR LA TAREA O SELECCIONAR UNA PRIORIDAD'}</p>`;
+
+        } else {
+            if (tarea == "" && miprioridad != "") {
+                selectAviso.innerHTML = `<p>${'TIENES QUE INGRESAR LA TAREA O SELECCIONAR UNA PRIORIDAD'}</p>`;
+            }
+        }
+
+        printListado(listaTareas);
+    }
+
+}
