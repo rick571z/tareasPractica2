782b6b237f4dfc1356d7130fdc8a039d50297429
Fire <wilmer_fi@hotmail.com>
2021-02-05 17:34:35 +0100

    se agrego los datoa al modelo.js

---
+++ js/modelo.js
@@ -0,0 +1,23 @@
+var listaTareas = new Array();
+
+listaTareas = [
+
+    {
+        idTarea: 1,
+        titulo: 'Estudiar JavaScript',
+        prioridad: 'urgente'
+    },
+
+    {
+        idTarea: 2,
+        titulo: 'Dormir',
+        prioridad: 'diaria'
+    },
+
+    {
+        idTarea: 3,
+        titulo: 'Salir a comer',
+        prioridad: 'mensual'
+    }
+
+];
